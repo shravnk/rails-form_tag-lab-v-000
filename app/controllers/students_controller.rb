04174cefffae65d035067791749024b83d37c0a4
Brendan O'Dea <brendan.ode4@gmail.com>
2018-04-15 21:07:25 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -9,8 +9,8 @@ class StudentsController < ApplicationController
   end
 
   def create
-    Post.create(title: params[:post][:title], description: params[:post][:description])
-    redirect_to posts_path
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path
   end
 
 end
